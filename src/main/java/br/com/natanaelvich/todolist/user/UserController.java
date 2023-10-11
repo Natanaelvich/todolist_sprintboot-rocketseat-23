9ab97a112df8868214be8196463fb73391747b18
natanaelvich <taelima1997@gmail.com>
2023-10-11 19:04:49 +0000

    update UserController

--- src/main/java/br/com/natanaelvich/todolist/user/UserController.java
+++ src/main/java/br/com/natanaelvich/todolist/user/UserController.java
@@ -2,44 +2,40 @@ package br.com.natanaelvich.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-/*
+
+
+/**
  * Modificador
- * public, private, protected
- * 
- * 
- * class, interface, enum
+ * public
+ * private
+ * protected
  */
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    /**
-     * String (texto)
-     * Integer (int) numeros inteiros
-     * Double (double) Números 0.0000
-     * Float (float) Números 0.0000 - maior numero de casas decimais
-     * char (A C)
-     * Date (data)
-     * void
-     */
+
     @Autowired
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public ResponseEntity create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
-        if(user != null){
-            System.out.println("Usuario ja existe");
-            return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Usuário já existe");
+        if (user != null) {
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        // TODO: add hash password
+        userModel.setPassword(userModel.getPassword());
+
         var userCreated = this.userRepository.save(userModel);
-        // System.out.println(userModel.getUsername());
-        return ResponseEntity.ok().body(userCreated);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
     }
 }
