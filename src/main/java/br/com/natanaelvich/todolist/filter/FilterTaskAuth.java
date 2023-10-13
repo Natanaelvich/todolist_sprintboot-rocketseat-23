f30396614b3ff9f16e45bf3933345a16fc762082
natanaelvich <taelima1997@gmail.com>
2023-10-13 22:23:44 +0000

    fix import

--- src/main/java/br/com/natanaelvich/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/natanaelvich/todolist/filter/FilterTaskAuth.java
@@ -1,13 +1,17 @@
+package br.com.natanaelvich.todolist.filter;
+
+import java.io.IOException;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import at.favre.lib.crypto.bcrypt.BCrypt;
-import br.com.rocketseat.todolist.user.IUserRepository;
+import br.com.natanaelvich.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
   @Autowired
